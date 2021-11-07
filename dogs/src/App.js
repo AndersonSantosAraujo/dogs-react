6ae2ccbc3e9fa2e55470fa1e33fc363927aeb1cb
AndersonSantosAraujo <anderson.amachinery@gmail.com>
2021-11-07 13:20:02 -0300

    8-4-Estrutura-Inicial

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
-import Api from "./api/Api";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Login from "./Components/Login/Login";
 
 const App = () => {
   return (
     <div>
-      <Api />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 };
