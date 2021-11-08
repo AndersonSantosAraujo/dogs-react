edab92a20e1a36ed46da9a894f836590f06efda7
AndersonSantosAraujo <anderson.amachinery@gmail.com>
2021-11-07 22:15:37 -0300

    8-10-userContext

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -5,17 +5,20 @@ import Home from "./Components/Home";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Login from "./Components/Login/Login";
+import { UserStorage } from "./UserContext";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
