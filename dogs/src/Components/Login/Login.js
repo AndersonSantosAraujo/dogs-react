4b46127c530aca8b2eeed60ba146b8ca3aaa5348
AndersonSantosAraujo <anderson.amachinery@gmail.com>
2021-11-08 22:00:52 -0300

    8-11-Validate-Token

--- dogs/src/Components/Login/Login.js
+++ dogs/src/Components/Login/Login.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import LoginForm from "./LoginForm";
 import LoginCreate from "./LoginCreate";
 import LoginPasswordLost from "./LoginPasswordLost";
 import LoginPasswordReset from "./LoginPasswordReset";
+import { UserContext } from "../../UserContext";
 
 const Login = () => {
+  const { login, setLogin } = React.useContext(UserContext);
+
+  if (login === true) return <Navigate to="/conta" />;
+
   return (
     <div>
       <Routes>
