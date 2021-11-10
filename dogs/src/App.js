de1ecceda41cbf22619035bef88237457a023ab2
Anderson <anderson.dos.santos.araujo@hotmail.com>
2021-11-10 18:42:26 -0300

    8-16-Protected-Route

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -5,7 +5,9 @@ import Home from "./Components/Home";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Login from "./Components/Login/Login";
+import User from "./Components/User/User";
 import { UserStorage } from "./UserContext";
+import ProtectedRoute from "./Components/Helper/ProtectedRoute";
 
 const App = () => {
   return (
@@ -15,7 +17,8 @@ const App = () => {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <ProtectedRoute path="conta/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>
---
+++ dogs/src/Components/User/User.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const User = () => {
+  return (
+    <div>
+      <di>Usuário</di>
+    </div>
+  );
+};
+
+export default User;
