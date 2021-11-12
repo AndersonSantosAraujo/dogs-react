884b7a1d559e1a191bade1ce89d9e8302a321588
Anderson <anderson.dos.santos.araujo@hotmail.com>
2021-11-11 21:22:14 -0300

    8-23-Comentario-Estilo

--- dogs/src/Components/Photo/PhotoCommentsForm.js
+++ dogs/src/Components/Photo/PhotoCommentsForm.js
@@ -3,6 +3,7 @@ import { COMMENT_POST } from "../../api";
 import { ReactComponent as Enviar } from "../../Assets/enviar.svg";
 import useFetch from "../../Hooks/useFetch";
 import Error from "../Helper/Error";
+import styles from "./PhotoCommentsForm.module.css";
 
 const PhotoCommentsForm = ({ id, setComments }) => {
   const { request, error } = useFetch();
@@ -17,15 +18,16 @@ const PhotoCommentsForm = ({ id, setComments }) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <textarea
+        className={styles.textarea}
         id="coment"
         name="coment"
         placeholder="Comente..."
         value={comment}
         onChange={({ target }) => setComment(target.value)}
       />
-      <button>
+      <button className={styles.button}>
         <Enviar />
       </button>
       <Error error={error} />
