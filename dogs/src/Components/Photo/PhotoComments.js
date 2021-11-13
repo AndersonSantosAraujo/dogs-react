cc1b387d7b6b1e0f536c35697f6d5829255b5859
Anderson <anderson.dos.santos.araujo@hotmail.com>
2021-11-13 13:53:31 -0300

    8-28-Foto-Pagina

--- dogs/src/Components/Photo/PhotoComments.js
+++ dogs/src/Components/Photo/PhotoComments.js
@@ -14,7 +14,10 @@ const PhotoComments = (props) => {
 
   return (
     <>
-      <ul ref={commentsSection} className={styles.comments}>
+      <ul
+        ref={commentsSection}
+        className={`${styles.comments} ${props.single ? styles.single : ""}`}
+      >
         {comments.map((comment) => (
           <li key={comment.comment_ID}>
             <b>{comment.comment_author}: </b>
@@ -23,7 +26,13 @@ const PhotoComments = (props) => {
         ))}
       </ul>
 
-      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+      {login && (
+        <PhotoCommentsForm
+          single={props.single}
+          id={props.id}
+          setComments={setComments}
+        />
+      )}
     </>
   );
 };
